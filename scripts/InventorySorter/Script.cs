f70629135052770fa60b0af02714eafa6150a771
Calvin <calvin@hearxgroup.com>
2024-05-11 14:13:32 +0200

    init Inventory sorter

---
+++ scripts/InventorySorter/Script.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+// Space Engineers DLLs
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using Sandbox.Game.EntityComponents;
+using VRageMath;
+using VRage;
+using VRage.Collections;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.Game.GUI.TextPanel;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ObjectBuilders.Definitions;
+using SpaceEngineers.Game.ModAPI.Ingame;
+
+/*
+ * Must be unique per each script project.
+ * Prevents collisions of multiple `class Program` declarations.
+ * Will be used to detect the ingame script region, whose name is the same.
+ */
+namespace InventorySorter {
+
+/*
+ * Do not change this declaration because this is the game requirement.
+ */
+public sealed class Program : MyGridProgram {
+
+    /*
+     * Must be same as the namespace. Will be used for automatic script export.
+     * The code inside this region is the ingame script.
+     */
+    #region InventorySorter
+
+    /*
+     * The constructor, called only once every session and always before any 
+     * other method is called. Use it to initialize your script. 
+     *    
+     * The constructor is optional and can be removed if not needed.
+     *
+     * It's recommended to set RuntimeInfo.UpdateFrequency here, which will 
+     * allow your script to run itself without a timer block.
+     */
+    public Program() {}
+
+    /*
+     * Called when the program needs to save its state. Use this method to save
+     * your state to the Storage field or some other means. 
+     * 
+     * This method is optional and can be removed if not needed.
+     */
+    public void Save() {}
+
+    /*
+     * The main entry point of the script, invoked every time one of the 
+     * programmable block's Run actions are invoked, or the script updates 
+     * itself. The updateSource argument describes where the update came from.
+     * 
+     * The method itself is required, but the arguments above can be removed 
+     * if not needed.
+     */
+    public void Main(string argument, UpdateType updateSource) {}
+
+    #endregion // InventorySorter
+}}
